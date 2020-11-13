c5ccd5928e57331399b45f0bc0a38f61d8500840
Luke Duncan <lduncan@intrahealth.org>
2020-11-13 05:58:24 -0700

    Fixed typo in gen-html script.

--- gen-html.sh
+++ gen-html.sh
@@ -1,1 +1,1 @@
-pandoc -r gfm --template=ihe_template --metadata title="HIE-Whitepaper" -metadata path-prefix="../../" -w html -o docs/index.html README.md
+pandoc -r gfm --template=ihe_template --metadata title="HIE-Whitepaper" --metadata path-prefix="../../" -w html -o index.html README.md
